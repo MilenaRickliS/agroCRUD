7c91cea309686a97fdd4aed7778ef5439e105218
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-09-27 20:15:16 -0300

    cc

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->where('id', '[0-9]+');
+Route::put('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
+Route::patch('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->where('id', '[0-9]+');
 
 require __DIR__.'/auth.php';
